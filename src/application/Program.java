4f58af5fc659a3082505fd953d7a5f3f034eb3b7
NatanFreitas <natanael_freitas@outlook.com>
2024-01-01 13:04:29 -0300

    Making the code leaner

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,8 @@ import model.entities.Seller;
 public class Program {
     public static void main(String[] args) {
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        
         Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
